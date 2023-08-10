d35585368cfe7da0dfb659771dee8d89851625e2
SuperSquiggs <132552863+SuperSquiggs@users.noreply.github.com>
2023-08-10 12:53:08 +1000

    Revert "Merge branch 'main' of https://github.com/SuperSquiggs/WK3"
    This reverts commit 1bb79136d022e9cf3adf38386c3ae3c17ff695d8, reversing
    changes made to 185d17bee1c0d8d6b673c347a68876c059e29004.

--- Assets/Custom Assets/Scripts/ExploderBomb.cs
+++ Assets/Custom Assets/Scripts/ExploderBomb.cs
@@ -3,98 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ExploderBomb : MonoBehaviour
-{
+public class ExploderBomb : MonoBehaviour {
 
-    public Transform bomb;
-    public float bombRadius;
-    public float bombPower;
-    public AudioSource bombSound;
-    public AudioSource buildUpSound;
+	public Transform player;								// reference to the playerś position
+	public Transform bomb;									// the position of the bomb object
+	public float bombRadius;								// extent the bomb force should affect
+	public float bombPower;									// force of the bomb
+	public AudioSource bombSound;							// what is a bomb without an explosion sound?
+	public Image bombFadeImage;								// reference to the UI white sprite image in front of the camera - for a nice blinding flash effect
 
-    public ParticleSystem partiFlame;
-    public Image bombFadeImage;
+	private ParticleSystem partiFlame;
+	private Vector3 explosionPos;
+	//private Color seeThrough;
+	private float fadeTime;									// time the white fadeout will take
 
-    private Transform player;
-    private Vector3 explosionPos;
-    private float holdDuration = 5f;
-    private bool isHolding = false;
-    private float currentTime = 0f;
-    private float fadeTime = 0.5f;
+	void Awake () {
+		bombFadeImage.enabled = false; 						// making sure the UI element is not visible when the game starts
+	}
 
-    void Awake()
-    {
-        bombFadeImage.enabled = false;
-    }
+	// Use this for initialization
+	void Start () {
+		explosionPos = bomb.position;
+		//seeThrough = new Color (1.0f, 1.0f, 1.0f, 0.0f);	// transparent
+		fadeTime = 0.5f;
+		if (player == null)
+			GameObject.Find("FPSController");				// grabbing a reference to the player just in case someone forgets to assign a reference in the Unity inspector
+		partiFlame = this.GetComponent<ParticleSystem>();
+	}
 
-    void Start()
-    {
-        explosionPos = bomb.position;
-        player = GameObject.Find("FPSController").transform;
-    }
+	void OnTriggerEnter (Collider other) {					// checking for a specific tagged object (Player) to collide with, not just any-old object
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isHolding = true;
-            currentTime = 0f;
-            buildUpSound.Play();
-        }
+		if (player && other.tag == "Player") {
+			//print ("BOMB");
+			Collider[] colliders = Physics.OverlapSphere (explosionPos, bombRadius);		// creating an array of colliders - anything in the OverlapSphere defined by the bomb radius from the explosion center
+			foreach (Collider hit in colliders) {											// iterating over the list of colliders found in the array 
+				Rigidbody rBody = hit.GetComponent<Rigidbody> ();							// temporarily assigning the rigidbody of the colliding object to a variable rBody
+				ParticleSystem partBall = hit.GetComponent<ParticleSystem> ();
+				if (rBody != null && rBody.tag == "ExplodingBalls") {						// checking we have an object on the correct layer - only things that exist and are tagged properly will be affected
+					rBody.AddExplosionForce (bombPower, explosionPos, bombRadius, 2.0f);	// adding an explosion force. The final var is an offset below the bomb center to give the effect more upward oomf.
+					if (partBall != null) {
+						partBall.Play ();
+					}
+				}
+					
+			}
+			bombSound.Play ();
+			bombFadeImage.enabled = true;			// turn on the white UI flash
+			partiFlame.Play();
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            isHolding = false;
-            currentTime = 0f;
-            buildUpSound.Stop();
-        }
+			CameraShake.Shake(0.75f, 0.25f); 		// duration, amount
+			Invoke ("BeginFade", 0.1f);				// start a function to fade the UI element - but keep it at full intensity for a brief time first
+		}
 
-        if (isHolding)
-        {
-            currentTime += Time.deltaTime;
+	}
 
-            if (currentTime >= holdDuration)
-            {
-                TriggerExplosion();
-                isHolding = false;
-                currentTime = 0f;
-            }
-        }
-    }
+	void BeginFade () {
+		bombFadeImage.CrossFadeAlpha (0.0f, fadeTime, true);	// use the crossfade function to fade the alpha value over time.
+		Invoke ("DisableFadeImage", fadeTime);					// start a function to restore the white UI elementś values and set it to invisible after it has faded out.
+	}
 
-    void TriggerExplosion()
-    {
-        Collider[] colliders = Physics.OverlapSphere(explosionPos, bombRadius);
-        foreach (Collider hit in colliders)
-        {
-            Rigidbody rBody = hit.GetComponent<Rigidbody>();
-            ParticleSystem partBall = hit.GetComponent<ParticleSystem>();
-            if (rBody != null && rBody.tag == "ExplodingBalls")
-            {
-                rBody.AddExplosionForce(bombPower, explosionPos, bombRadius, 2.0f);
-                if (partBall != null)
-                {
-                    partBall.Play();
-                }
-            }
-        }
-
-        bombSound.Play();
-        bombFadeImage.enabled = true;
-        partiFlame.Play();
-        CameraShake.Shake(0.75f, 0.25f);
-        Invoke("BeginFade", 0.1f);
-    }
-
-    void BeginFade()
-    {
-        bombFadeImage.CrossFadeAlpha(0.0f, fadeTime, true);
-        Invoke("DisableFadeImage", fadeTime);
-    }
-
-    void DisableFadeImage()
-    {
-        bombFadeImage.enabled = false;
-        bombFadeImage.CrossFadeAlpha(1.0f, 0.01f, true);
-    }
+	void DisableFadeImage () {
+		bombFadeImage.enabled = false;
+		bombFadeImage.CrossFadeAlpha (1.0f, 0.01f, true);
+	}
 }
